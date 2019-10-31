098bd9df9430eebc29b428236cd7de2795bce0a5
antonybstack <ablyakhe@uncc.edu>
2019-10-31 02:58:14 +0000

    renamed everything from 'welcome' to 'login' which makes more sense based on file names.

--- RateMyCCI-App/config/routes.rb
+++ RateMyCCI-App/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#login'
+  root 'login#login'
   
   get '/classes' => 'classes#classes'
-  get '/welcome' => 'welcome#login'
+  get '/login' => 'login#login'
 end
