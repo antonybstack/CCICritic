fb357f958a6f59099e0870ce790b4c4fc31a5c56
EC2 Default User <ec2-user@ip-172-31-42-158.us-east-2.compute.internal>
2019-12-10 00:59:30 +0000

    backup before course data fields changes

--- webapp/app/controllers/reviews_controller.rb
+++ webapp/app/controllers/reviews_controller.rb
@@ -1,2 +1,19 @@
 class ReviewsController < ApplicationController
+    def create
+        @course = Course.find(params[:course_id])
+        @review = @course.reviews.create(review_params)
+        redirect_to course_path(@course)
+    end
+    
+    def destroy
+       @course = Course.find(params[:course_id])
+       @review = @course.reviews.find(params[:id])
+       @review.destroy
+       redirect_to course_path(@course)
+    end
+    
+    private
+    def review_params
+        params.require(:review).permit(:professor_name, :overall_rating, :professor_rating, :difficulty_rating, :language, :textbook_req, :computer_req, :presentations, :tests, :essays, :comment)
+    end
 end
